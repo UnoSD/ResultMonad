2bc54b5eef6bc7cadc1917ae8df1c1228ec134a5
UnoSD <unosd.dev@gmail.com>
2018-08-19 15:18:22 +0100

    Mixed test (and possible duplicate methods in TAskResultExtensions)

---
+++ Result/TaskResultExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Result;
+
+namespace Monad.TaskResult
+{
+    static class TaskResultExtensions
+    {
+        internal static Task<IResult<TResult>> SelectMany<T, TResult>
+        (
+            this Task<T> source,
+            Func<T, IResult<TResult>> func
+        ) => source.Map(func);
+
+        internal static Task<IResult<TOutput>> SelectMany<T, TResult, TOutput>
+        (
+            this Task<T> source,
+            Func<T, IResult<TResult>> func,
+            Func<T, TResult, TOutput> project
+        ) => source.Map(tres => func(tres).Map(fres => project(tres, fres)));
+
+        internal static Task<IResult<TResult>> Select<T, TResult>
+        (
+            this IResult<T> source,
+            Func<T, Task<TResult>> func
+        ) => source.MapAsync(func);
+
+        internal static Task<IResult<TResult>> MapAsync<T, TResult>
+        (
+            this IResult<T> source,
+            Func<T, Task<TResult>> func
+        ) => source.Match(result => func(result).Map(o => o.ToResult()),
+                          error => error.ToFailureResult<TResult>()
+                                        .ToTask());
+    }
+}
---
+++ Test/ResultTaskTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using Monad.TaskResult;
+using static Result.ResultExtensions;
+
+namespace Test
+{
+    [TestFixture]
+    class ResultTaskTest
+    {
+        [Test]
+        public void MonadResult()
+        {
+            var query =
+                from i in "pippo".ToTask()
+                from p in (i + "peppe").ToResult()
+                select p;
+
+            var chain =
+                "pippo".ToTask()
+                       .SelectMany(i => (i + "peppe").ToResult());
+
+            Assert.That(query.Result(), Is.EqualTo(chain.Result()));
+            Assert.That(chain.Result(), Is.EqualTo("pippopeppe"));
+        }
+    }
+}
